44d73e0c70e0b8259db5716ef1fbd533b9a4141f
MichaelAlexander-07 <michaelAlexander81@hotmail.com>
2025-08-01 14:24:53 -0300

    mejoras del carrito

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,45 @@
-import { useReducer } from "react";
-import { Guitar } from "./components/Guitar";
-import { Header } from "./components/Header";
-import useCart from "./hooks/useCart.ts";
-import { cartReducer, initialState } from "./reducers/cart-reducer.ts";
+import { useReducer, useEffect } from "react"
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { cartReducer, initialState } from "./reducers/cart-reducer"
 
-const App = () => {
+function App() {
 
-  const [state, dispatch] = useReducer(cartReducer, initialState);
-  const {
+  const [state, dispatch] = useReducer(cartReducer, initialState)
 
-    cart,
-    addToCart,
-    removeFromCart,
-    incrementQuantity,
-    decrementQuantity,
-    clearCart,
-    isEmpty,
-    cartTotal
-  } = useCart();
+  useEffect(() => {
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
 
   return (
     <>
-      <Header
-        cart={cart}
-        removeFromCart={removeFromCart}
-        incrementQuantity={incrementQuantity}
-        decrementQuantity={decrementQuantity}
-        clearCart={clearCart}
-        isEmpty={isEmpty}
-        cartTotal={cartTotal}
+      <Header 
+        cart={state.cart}
+        dispatch={dispatch}
       />
+      
       <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+          <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">
-          {state.data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} dispatch={dispatch} />
-          ))}
-        </div>
+          <div className="row mt-5">
+              {state.data.map((guitar) => (
+                  <Guitar 
+                    key={guitar.id}
+                    guitar={guitar}
+                    dispatch={dispatch}
+                  />
+              ))}
+          </div>
       </main>
 
+
       <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
-          </p>
-        </div>
+          <div className="container-xl">
+              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          </div>
       </footer>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,40 +1,31 @@
-import type { CartActions } from '../reducers/cart-reducer';
-import type {Guitar as GuitarType} from'../types';
+import { Dispatch } from 'react'
+import type { Guitar } from '../types'
+import type { CartActions } from '../reducers/cart-reducer'
 
 type GuitarProps = {
-    guitar: GuitarType;
-    dispatch: React.Dispatch<CartActions>;
+    guitar : Guitar, 
+    dispatch: Dispatch<CartActions>
 }
 
+export default function Guitar({guitar, dispatch} : GuitarProps) {
 
-export const Guitar = ({guitar, dispatch}: GuitarProps) => {
-const { name, description, price, image } = guitar;
+    const { name, image, description, price } = guitar
 
-
-  return (
-    <>
-      <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-        <div className="col-4">
-          <img
-            className="img-fluid"
-            src={`/img/${image}.jpg`}
-            alt="imagen guitarra"
-          />
+    return (
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
+            </div>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-black text-primary fs-3">${price}</p>
+                <button 
+                    type="button"
+                    className="btn btn-dark w-100"
+                    onClick={() => dispatch({type: 'add-to-cart', payload: {item: guitar}})}
+                >Agregar al Carrito</button>
+            </div>
         </div>
-        <div className="col-8">
-          <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
-          <p>{description}</p>
-          <p className="fw-black text-primary fs-3">${price}</p>
-          <button 
-          type="button" 
-          className="btn btn-dark w-100"
-          onClick={()=>dispatch({ type: 'ADD_TO_CART', payload: { item: guitar } })}
-          
-          >
-            Agregar al Carrito
-          </button>
-        </div>
-      </div>
-    </>
-  );
-};
+    )
+}
